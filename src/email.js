cd58a3ff948551aafe930e79a4b26f615db80137
SapphireGaze <jasminehuang2048@outlook.com>
2023-08-06 23:20:45 -0700

    Initial Commit:  - Finished modal design/logic  - Finished sending verification email via smtp server  - Assign role if member email is verified

---
+++ src/email.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const nodemailer = require('nodemailer');
+
+const { emailAddress } = require('../config.json');
+
+// get verification email html content and load content 
+let html = fs.readFileSync('./templates/verification.html').toString();
+const $ = require('cheerio').load(html);
+
+// create transporter for nodemailer
+const transporter = nodemailer.createTransport({
+	host: 'localhost',
+	port: 587,
+	requireTLS: true,
+	secure: false,
+	tls: {
+		rejectUnauthorized: false
+	},
+});
+
+// generate 6 digits random code
+function generateCode() {
+	return Math.floor(Math.random() * (999999 - 100000) + 100000);
+}
+
+// replace default 000000 code in html page with random generated code
+function modifyHTML() {
+	const replaceRegex = /000000/;
+	const code = generateCode();
+
+	const getTextNodes = (elem) => elem.type === 'text'?[]:
+        elem.contents().toArray()
+        .filter(el => el !== undefined)
+        .reduce((acc, el) =>
+            acc.concat(...el.type === 'text' ? [el] : getTextNodes($(el))), []);
+
+	getTextNodes($('html'))
+    	.filter(node => $.html(node).match(replaceRegex))
+    	.map(node => $(node).replaceWith($.html(node).replace(replaceRegex, `${code}`)));
+
+	html = $.html();
+
+	return code;
+}
+
+// send email to passed in parameter address with modified verification html and return generated code
+module.exports.verifyEmail = async function (email) {
+	const code = modifyHTML();
+
+	const info = await transporter.sendMail({
+		from: emailAddress,
+		to: email,
+		subject: 'Discord Member Verification Code',
+		html: html,
+	});
+	console.log(`Message sent to ${email}: ${info.messageId} with the verification code ${code}.\n`);
+
+	return code;
+}
---
+++ src/index.js
@@ -0,0 +1,217 @@
+const fs = require('fs');
+const {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    Client,
+    Events,
+    GatewayIntentBits,
+    InteractionType,
+    ModalBuilder,
+    TextInputBuilder,
+    TextInputStyle,
+    EmbedBuilder,
+} = require('discord.js');
+
+const email = require('./email.js');
+
+const { token, channelId, roleId, allowedDomains } = require('../config.json');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+// generate a 6 digits random code
+let generatedCode = Math.floor(Math.random() * (999999 - 100000) + 100000);
+
+// create initial message and embed
+client.on(Events.MessageCreate, (message) => {
+    // return if message isn't 'verify' or isn't in intended channel
+    if (message.channel.id != channelId || message.content != 'verify') return;
+
+    // embed initial description content, change to whatever
+    const agreement = fs.readFileSync('./templates/agreement.txt').toString();
+
+    // build initial button
+    const InitialButton = new ActionRowBuilder();
+    InitialButton.addComponents(
+        new ButtonBuilder()
+            .setCustomId('initial-button')
+            .setStyle(ButtonStyle.Primary)
+            .setLabel('Active Member Verification'),
+    );
+    
+    // build initial embed
+    const InitialEmbed = new EmbedBuilder()
+        .setColor(0xCFC2E9)
+        .setTitle('Active Member Verification')
+        .setURL('https://github.com/SapphireGaze/discord-verification')
+        .setAuthor({
+            name: 'Member Verification Bot', 
+            iconURL: 'https://logodix.com/logo/557580.png', 
+        })
+        .setDescription(agreement);
+    
+    // reply with embed and button
+    message.reply({
+        embeds: [InitialEmbed],
+        components: [InitialButton],
+    });
+});
+
+// called on every interaction
+client.on(Events.InteractionCreate, async (interaction) => {
+    // return if interaction user already have role
+    if (interaction.member.roles.cache.some(role => role.id === roleId)) {
+        interaction.reply({
+            content: 'You have already completed Active Member Verification!', 
+            ephemeral: true,
+        });
+        return;
+    }
+
+    // check if interation is a button click
+    if (interaction.isButton()) {
+        // check id of button interaction to see if it's initial button
+        if (interaction.customId === 'initial-button') {
+            // build initial modal
+            const InitialModal = new ModalBuilder()
+                .setCustomId('initial-modal')
+                .setTitle('Active Member Verification')
+                .addComponents([
+                    new ActionRowBuilder().addComponents(
+                        new TextInputBuilder()
+                            .setCustomId('email-input')
+                            .setLabel('University Email')
+                            .setStyle(TextInputStyle.Short)
+                            .setPlaceholder('E.g. adalovelace@universitydomain.edu')
+                            .setRequired(true),
+                    ),
+                    new ActionRowBuilder().addComponents(
+                        new TextInputBuilder()
+                            .setCustomId('signature-input')
+                            .setLabel('Signature')
+                            .setStyle(TextInputStyle.Short)
+                            .setPlaceholder('E.g. Ada Lovelace')
+                            .setRequired(true),
+                    ),
+                ]);
+
+            await interaction.showModal(InitialModal);
+        }
+
+        // check id of button interaction to see if it's email verification button
+        if (interaction.customId === 'email-verification-button') {
+            // build email verification modal
+            const EmailModal = new ModalBuilder()
+                .setCustomId('email-verification-modal')
+                .setTitle('Email Verification')
+                .addComponents([
+                    new ActionRowBuilder().addComponents(
+                        new TextInputBuilder()
+                            .setCustomId('code-input')
+                            .setLabel('Email Verification Code')
+                            .setStyle(TextInputStyle.Short)
+                            .setPlaceholder('E.g. 123456')
+                            .setRequired(true),
+                    ),
+                ]);
+            
+            await interaction.showModal(EmailModal);
+        }
+    }
+
+    // check if interaction type is modal submit
+    if (interaction.type === InteractionType.ModalSubmit) {
+        // check if the modal submit is from initial modal
+        if (interaction.customId === 'initial-modal') {
+            // obtain input from modal fields
+            const emailInput = interaction.fields.getTextInputValue('email-input').trim();
+            const signatureInput = interaction.fields.getTextInputValue('signature-input').trim();
+
+            // parse domain from user email input
+            const domain = emailInput.split('@')[1];
+
+            // list of acceptable domains
+            const AcceptedDomains = new Set(allowedDomains);
+
+            // return if user email isn't from an acceptable domain
+            if (!AcceptedDomains.has(domain)) {
+                interaction.reply({
+                    content: 'Please resubmit the form with your university email.', 
+                    ephemeral: true,
+                });
+                return;
+            }
+
+            // build email verification button
+            const EmailButton = new ActionRowBuilder();
+            EmailButton.addComponents(
+                new ButtonBuilder()
+                    .setCustomId('email-verification-button')
+                    .setStyle(ButtonStyle.Success)
+                    .setLabel('Enter Email Verification Code'),
+            );
+            
+            // build email verification info embed
+            const EmailEmbed = new EmbedBuilder()
+                .setColor(0xCFC2E9)
+                .setTitle('Email Verification')
+                .setURL('https://github.com/SapphireGaze/discord-verification')
+                .setAuthor({
+                    name: 'Member Verification Bot', 
+                    iconURL: 'https://logodix.com/logo/557580.png', 
+                })
+                .setDescription('You will receive an email with a code shortly. ' +
+                    'Please enter the code below to confirm your information is correct.')
+                .addFields(
+                    { name: 'Email', value: `${emailInput}` },
+                    { name: 'Signature', value: `${signatureInput}` },
+                )
+            
+            // send email to user input email and set generatedCode to randomly generated code from email
+            generatedCode = await email.verifyEmail(emailInput).catch(console.error);
+            
+            interaction.reply({
+                embeds: [EmailEmbed],
+                components: [EmailButton], 
+                ephemeral: true,
+            });
+        }
+
+        // check if the modal submit is from email verification modal
+        if (interaction.customId === 'email-verification-modal') {
+            const codeInput = interaction.fields.getTextInputValue('code-input').trim();
+
+            // compare generated code with user input code
+            // if correct, assign role and reset generated code
+            if (generatedCode == codeInput) {
+                interaction.member.roles.add(roleId);
+                console.log(`${interaction.member.user.username} is now an Active Member.\n`);
+
+                generatedCode = Math.floor(Math.random() * (999999 - 100000) + 100000);
+
+                interaction.reply({
+                    content: 'You have completed Active Member Verification and unlocked channels!', 
+                    ephemeral: true,
+                });
+                return;
+            }
+
+            interaction.reply({
+                content: `Your verification code is incorrect, please try again!`, 
+                ephemeral: true,
+            });
+        }
+    }
+});
+
+client.on(Events.ClientReady, () => {
+    console.log(`Currently logged in as ${client.user.tag}!\n`);
+});
+
+client.login(token);
